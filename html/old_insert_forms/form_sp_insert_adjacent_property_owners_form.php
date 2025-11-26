dd556406a061658e3c9dc44b68b2292b73944a51
Calvin Mar <calvin.mar@centre.edu>
2025-11-26 15:40:21 +0000

    Removing Error messages.

--- html/old_insert_forms/form_sp_insert_adjacent_property_owners_form.php
+++ html/old_insert_forms/form_sp_insert_adjacent_property_owners_form.php
@@ -1,9 +1,5 @@
 <?php
 // Show all errors (for development; consider removing in production)
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 require_once 'config.php';
 requireLogin();
